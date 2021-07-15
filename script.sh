8f0749838c29adbecb104b1cc81aab28a73f86c5
Asheeshkrsharma <proof1234@gmail.com>
2021-07-15 07:25:28 +0530

    Initial commit

--- script.sh
+++ script.sh
@@ -1,5 +1,3 @@
 curl https://raw.githubusercontent.com/jpatokal/openflights/master/data/airports.dat > airports.txt
-
-awk -F "\"*,\"*" '{print $7,$8,$9 > $6}' airports.txt
-
+awk -F "\"*,\"*" '{ORS=""; print "\x22"$2"\x22",$7,$8,$9 * 0.3048 > $6}' OFMT="%.4f" airports.txt
 rm -rf airports.txt
